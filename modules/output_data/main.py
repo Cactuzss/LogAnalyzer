b057a73ca977701a582d3027ee4cec0f5a239d49
DanTar <tarasovdanil2207@gmail.com>
2025-05-17 17:04:32 +0400

    add collect final data module

---
+++ modules/output_data/main.py
@@ -0,0 +1,91 @@
+import regex, asyncio
+from modules import CachingJSON
+
+
+jc = CachingJSON()
+regex_for_url = r'(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?'
+
+def collect_data(data:list[str], claster_name:str, links:list[str], classifire:LabelClassifier):
+    """
+    dictionary format:
+    {
+        "claster_name":[{
+            "link":"text_log"
+        }...]
+
+    }
+    """
+    try:
+        if not isinstance(classifire):
+            raise Exception("non valid data")
+        if not isinstance(data, type(list[str])):
+            raise Exception("non valid data")
+        if not isinstance(claster_name, str):
+            raise Exception("non valid data")
+        if not isinstance(links, list[str]):
+            raise Exception("non valid data")
+
+        for link in data.get("list_links"):
+            if regex.match(regex_for_url, link):
+                raise Exception("non valid data")
+        if len(links) != len(data):
+            raise Exception("dont correct data")
+        
+        # temp_obj = {
+        #     "claster_name":claster_name,
+        #     "data":[
+
+        #     ]
+        # }
+
+        # for i in range(0, len(links)):
+        #     temp_obj.get("data").append({link[i]: data[i]})
+
+
+        # asyncio.run(jc.putting('temp', "claster", temp_obj))
+
+        # request to lrm
+        generate_data = classifire.generate_log_description(data, claster_name)
+        """
+        {
+        err_msg:,
+        err_exp:,
+        err_sol:
+        }
+        """
+
+
+        result_data = {
+            "claster_name":claster_name,
+            "generate_data":generate_data,
+            "data":[]
+        }
+
+        for i in range(0, len(links)):
+            result_data.get("data").append({
+                "link":links[i],
+                "log_text":data[i]
+            })
+        """
+        {
+            "claster_name":claster_name,
+            "generatedata":{
+                "msg":msg,
+                "exp":exp,
+                "solve":solve
+            }
+            data:[{
+                    "link":link
+                    "log_text":log_text
+                }...
+            ]
+        }
+        """
+        return result_data
+
+
+
+
+    except Exception as e:
+        print(e)
+
